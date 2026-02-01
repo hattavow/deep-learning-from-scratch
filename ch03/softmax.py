71257c1ac978e80b45e858cf4bbd8f54da89f31b
hattavow <rikuhatano09@gmail.com>
2026-02-02 00:30:00 +0900

    Add softmax function implementation

---
+++ ch03/softmax.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def softmax(a):
+    # Avoid overflow
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+
+print(y)
+print(np.sum(y))
