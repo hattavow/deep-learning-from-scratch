350fc4f932fedcf1aa84eae6876e59c056c2029a
hattavow <rikuhatano09@gmail.com>
2026-02-03 00:25:04 +0900

    Add MNIST dataset loading and preprocessing functionality

---
+++ ch03/mnist_show.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sys.path.append(os.pardir)
+import numpy as np
+from PIL import Image
+
+from dataset.mnist import load_mnist
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+img = x_train[0]
+label = t_train[0]
+print("label:", label)
+
+print("img shape:", img.shape)
+img = img.reshape(28, 28)
+print("reshaped img shape:", img.shape)
+
+img_show(img)
