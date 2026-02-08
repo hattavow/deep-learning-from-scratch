fa3add43ab4766b51d2b2503d85512ca62027b49
hattavow <rikuhatano09@gmail.com>
2026-02-08 11:41:21 +0900

    Refactor training script to initialize accuracy lists and iter_per_epoch before training loop

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -5,13 +5,17 @@ from dataset.mnist import load_mnist
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
 
 # Hyperparameters
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -31,3 +35,12 @@ for i in range(iters_num):
     # Record loss
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(
+            f"Epoch {i // iter_per_epoch}: Train Accuracy: {train_acc}, Test Accuracy: {test_acc}"
+        )
