9deb1057256919f044a4e597b1976f02d36b3c77
hattavow <rikuhatano09@gmail.com>
2026-03-10 23:06:02 +0900

    Add softmax with loss layer.

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from common.functions import cross_entropy_error, softmax
+
 
 class Relu:
     def __init__(self):
@@ -48,5 +50,25 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師データ (one-hot vector)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
